0a4569644856a1080c86275709ad2df8124e0204
Kenneth A. Jones II <kenneth@nvnctechnology.com>
2017-01-24 00:11:53 -0600

    wip

--- setuptemplate.py
+++ setuptemplate.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-from subprocess import call
+import os
+import sys
+import subprocess
+import logging
+import shutil
 
 ########################################################################################################################
 # CONFIGURATION SECTION                                                                                                #
 ########################################################################################################################
 
+logging.basicConfig(filename="install.log",
+                    filemode='w',
+                    format='[%(levelname)-8s] %(message)s',
+                    level=logging.DEBUG)
+
 # This is the working directory for setup script
-WORKING_DIR = '/home/pi/'
+WORKING_DIR = '/Users/kenneth/projects/RetropieSetupTemplate/root/home/pi/'
 
 # Username and repository name.  This will used to generate a URL for downloading the repository
-REPO_USERNAME = "git_username"
-REPO_NAME = "reponame"
+REPO_USERNAME = "kjones200"
+REPO_NAME = "testrepo"
 
 # List of files requiring modification.  List is comprised of dictionary containing the filename
 # location, search tet, and the modification to apply.  For example ('file.txt', '/home/pi/')
@@ -28,19 +37,47 @@ COPY_LIST = [
 
 ]
 
+
 ########################################################################################################################
 # EXECUTION SECTION                                                                                                    #
 ########################################################################################################################
 
-# Change to the working director before doing anything
-call('cd ' + WORKING_DIR)
+def main():
+    # Change to the working director before doing anything
+    os.chdir(WORKING_DIR)
+    
+    # Update dependency list and download necessary dependencies.  Uses the -y option for no prompts
+    # out,err = run_process(['sudo apt-get update'])
+    # out, err = run_process (['sudo apt-get -y install python-dev python-pip python-gpiozero'])
+    
+    # Downloads necessary python modules dependencies.  Use 'yes' for installation without prompts.
+    # code = run_process(['yes |', 'pip install psutil pyserial'])
+    # if code != 0:
+    #    exit(code)
+    
+    # Download the repository
+    # Check for repo and delete it to prevent errors when downloading due to repo already existing
+    if os.path.exists(os.path.join(os.getcwd(), REPO_NAME)):
+        shutil.rmtree(os.path.join(os.getcwd(), REPO_NAME), ignore_errors=True)
+    
+    run_process(['git', 'clone https://github.com/%s/%s.git' % (REPO_USERNAME, REPO_NAME)])
+    os.chdir(REPO_NAME) # move into repo directory
 
-# Update dependency list and download necessary dependencies.  Uses the -y option for no prompts
-call('sudo apt-get update && sudo apt-get -y install python-dev python-pip python-gpiozero')
 
-# Downloads necessary python modules dependencies.  Use 'yes' for installation without prompts.
-call('yes | sudo pip install psutil pyserial')
+def run_process(cmd):
+    p = subprocess.Popen(' '.join(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while p.poll() is None:
+        l = p.stdout.readline()  # This blocks until it receives a newline.
+        if l != "":
+            print l
+    # When the subprocess terminates there might be unconsumed output
+    # that still needs to be processed.
+    out = p.stdout.read()
+    err = p.stderr.read()
+    if p.returncode != 0:
+        logging.error(err)
+    return p.returncode
 
-# Download the repository
-call('git clone https://github.com/%s/%s.git' % (REPO_USERNAME, REPO_NAME))
-call('cd %s' % REPO_NAME)  # move into repo directory
+
+if __name__ == '__main__':
+    main()
